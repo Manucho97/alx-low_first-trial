6d6ccbeedc7a3bcf5c013fc328276bd8fbcb7274
manucho97 <emmanuelmukaisimasai@gmail.com>
2022-09-08 19:18:55 -0700

    k

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Main-starts the program
+ *
+ * Return- returns an integer
+ *
+ */
+
+}
+
+int main(void)
+	puts("\"Programming is like building a multilingual  puzzle");
+	return (0);
+
+}
