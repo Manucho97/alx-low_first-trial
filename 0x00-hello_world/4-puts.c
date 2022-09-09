73895963ca9d138d56c22e50c0382653a0a4eb99
manucho97 <emmanuelmukaisimasai@gmail.com>
2022-09-08 20:10:04 -0700

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * Main - starts the program
+ * Main - initiates the program
  *
- * Return: Returns an integer
+ * Retun: return  o
  */
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{
+	puts("\"Programming is like building a multiligual puzzzle");
+	retiurn (0);
 }
