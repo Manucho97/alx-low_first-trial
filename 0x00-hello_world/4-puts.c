3f20ddef982e8cc58c1c9ccd236ede10639f8ea6
manucho97 <emmanuelmukaisimasai@gmail.com>
2022-09-12 19:03:20 -0700

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
-#include <stdio.h>
+#incclude <stdio.h>
 /**
- * Main - initiates the program
  *
- * Retun: return  o
- */
-int main(void)
-{
-	puts("\"Programming is like building a multiligual puzzzle");
-	return (0);
-}
+ *
+ *
+ *
---
+++ 0x00-hello_world/if.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - initiates
+ *
+ * return: returns an integer
+ */
+int main(void)
+{
+	int x = 18;
+	int y = 20;
+	if (x > y) {
+		printf("x is greater than Y,");
+	} else {
+		printf("fuck you,\n");
+	}
+	return 0;
+}
