950910bf62f2b8f632b4e183b13eb5b58940e06f
manucho97 <emmanuelmukaisimasai@gmail.com>
2022-09-16 05:46:46 -0700

    j

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,16 @@
-#incclude <stdio.h>
+#include <stdio.h>
 /**
+ *main - starts the code
  *
+ *Return: returns an integer 0
  *
- *
- *
+ */
+
+int main(void)
+{
+	char mystr[] = "programming is like building a multilingual puzzle";
+
+	puts(mystr);
+
+	return (0);
+}
