8299d26536044c1a00f93537b98bc4c2204a92a4
manucho97 <emmanuelmukaisimasai@gmail.com>
2022-09-16 14:21:10 -0700

    j

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/** 
+ * main - Prints to standard error
+ *
+ * Return: Always 1 (successs)
+ */
+
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dorar Korpar, 2015-10-19\n", 59);
+	
+	return (1);
+}
