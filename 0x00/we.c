b6a32382687da502f028685d0f53db4a17794853
manucho97 <emmanuelmukaisimasai@gmail.com>
2022-09-16 16:04:31 -0700

    o

---
+++ 0x00/we.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		i++;
+		printf("%d\n", i / 2);
+		
+	}
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * a: first number 1st integer.
+ *
+ * b: second interger.
+ *
+ * Return: multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
