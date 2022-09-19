7dd6c18be3b72327781c315c2acebe175c58b3a7
manucho97 <emmanuelmukaisimasai@gmail.com>
2022-09-18 19:10:10 -0700

    k

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
-/** 
+/**
  * main - Prints to standard error
  *
  * Return: Always 1 (successs)
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dorar Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
