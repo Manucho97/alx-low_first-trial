29c6931525760b17a48d490a2a36ebb8a4516bbc
manucho97 <emmanuelmukaisimasai@gmail.com>
2022-10-10 04:35:29 -0700

    testing

---
+++ 0x01-variables_if_else_while/ream.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - counts from 1
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for(num = 0; num <= 20; num++)
+	{
+		printf("\n");
+		printf("%d", num);
+		printf(",");
+		printf(" ");
+	}
+	return (0);
+}
